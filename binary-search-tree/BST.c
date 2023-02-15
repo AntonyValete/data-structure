d63851cab409a270ee4c210087d2f4d95daf2b51
Antony Valete <95865989+AntonyValete@users.noreply.github.com>
2023-02-15 21:58:48 +0000

    Creating create node function

--- binary-search-tree/BST.c
+++ binary-search-tree/BST.c
@@ -7,6 +7,15 @@ typedef struct node {
     struct node *left, *right;
 } node;
 
+node *create_new_node(int data) {
+    node *dummy = (node *) malloc(sizeof(node));
+
+    dummy->data = data;
+    dummy->left = dummy->right = NULL;
+
+    return dummy;
+}
+
 void inorder_tree_walk(node *root) {
     if (root != NULL) {
         inorder_tree_walk(root->left);
