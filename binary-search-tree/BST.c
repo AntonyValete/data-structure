2c541537b248ec0fdbad792d25e74c1c73ddcb13
Antony Valete <95865989+AntonyValete@users.noreply.github.com>
2023-02-15 21:56:57 +0000

    Fixing struct name.

--- binary-search-tree/BST.c
+++ binary-search-tree/BST.c
@@ -7,20 +7,6 @@ typedef struct node {
     struct node *left, *right;
 } node;
 
-node *insert_node(node *tree, int data) {
-    if (tree == NULL)
-        return create_new_node(data);
-
-    if (tree->data > data)
-        tree->left = insert_node(tree->left, data);
-    else if (tree->data < data)
-        tree->right = insert_node(tree->right, data);
-    else
-        error_handler(1);
-
-    return tree;
-}
-
 void inorder_tree_walk(node *root) {
     if (root != NULL) {
         inorder_tree_walk(root->left);
